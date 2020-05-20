5eadea1cbf983f3e9666f444af69085afe0a997a
Stepess <stepanersh@gmail.com>
2020-05-20 19:38:42 +0300

    fix app startup

---
+++ src/main/java/ua/stepess/dnipro/orderservice/config/PropertiesConfiguration.java
@@ -0,0 +1,9 @@
+package ua.stepess.dnipro.orderservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan
+public class PropertiesConfiguration {
+}
