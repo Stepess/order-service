b021477ca06699d8e0f4ccb6ddabea3781272857
Stepess <stepanersh@gmail.com>
2020-05-20 20:36:19 +0300

    logging

---
+++ src/main/java/ua/stepess/dnipro/orderservice/config/RestTemplateConfiguration.java
@@ -0,0 +1,16 @@
+package ua.stepess.dnipro.orderservice.config;
+
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ua.stepess.dnipro.orderservice.config.rest.LoggingRestTemplateCustomizer;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    public RestTemplateCustomizer loggingRestTemplateBuilder() {
+        return new LoggingRestTemplateCustomizer();
+    }
+
+}
